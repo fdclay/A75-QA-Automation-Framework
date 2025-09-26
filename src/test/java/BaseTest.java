d26cda2f8ce5c05c835267eef21b0530c3d641b6
fdclay <felicia.clay@testpro.io>
2025-09-25 22:09:58 -0400

    Added steps to add to playlist

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,11 +1,101 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
-public class BaseTest {
+import java.time.Duration;
 
+public class BaseTest {
+    public WebDriver driver;
+    public String url;
     @BeforeSuite
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
+    }
+
+@BeforeMethod
+    public void setupBrowser(){
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        //preconditions
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+        naviagionURL();
+
+    }
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
+    }
+
+    public void clickSubmitBtn() {
+        WebElement submitBtn = driver.findElement(By.cssSelector("button[type='submit']"));
+        submitBtn.click();
+    }
+
+        public void providePassword(String password) {
+        WebElement passwordField = driver.findElement(By.cssSelector("input[type='password']"));
+        passwordField.clear();
+        passwordField.sendKeys(password);
+    }
+
+    public void provideEmail(String email) {
+        WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
+        emailField.clear();
+        emailField.sendKeys(email);
+    }
+    public void naviagionURL(){
+        url = "https://qa.koel.app/";
+        driver.get(url);
+    }
+
+    public String getAddToPlaylistSuccessMsg() {
+        //Alert alert = driver.switchTo().alert();
+        WebElement notification = driver.findElement(By.cssSelector("div.success.show"));
+        return notification.getText();
+
+        //<div class="success show">Added 1 song into "Test Pro Playlist."</div>
+    }
+
+    public void choosePlaylist() {
+        WebElement playlist = driver.findElement(By.xpath("//section[@id='songResultsWrapper']//li[contains(text(),'Test Pro Playlist')]"));
+        playlist.click();
+    }
+
+    public void createPlaylist(String newlist) {
+        WebElement newplaylist = driver.findElement(By.xpath("//*[@id=\"songsWrapper\"]/header/div[3]/div/section[2]/form/input"));
+        newplaylist.clear();
+        newplaylist.sendKeys(newlist);
+    }
+    public void clickSaveBtn() {
+        WebElement saveBtn = driver.findElement(By.xpath("//*[@id=\"songsWrapper\"]/header/div[3]/div/section[2]/form/button"));
+        saveBtn.click();
+    }
+    public void clickAddToBtn() {
+        WebElement addToBtn = driver.findElement(By.xpath("//section[@id='songResultsWrapper']//button[@class='btn-add-to']"));
+        addToBtn.click();
+    }
+
+    public void selectFirstSong() {
+        WebElement Firstsong = driver.findElement(By.xpath("//section[@id='songResultsWrapper']//tr[@class='song-item'][1]"));
+        Firstsong.click();
+    }
+
+    public void clickViewALLBtn() {
+        WebElement viewAll = driver.findElement(By.xpath("//button[@data-test='view-all-songs-btn']"));
+        viewAll.click();
+    }
+
+    public void searchSong(String song) {
+        WebElement searchField = driver.findElement(By.cssSelector("input[type='search']"));
+        searchField.clear();
+        searchField.sendKeys(song);
     }
 }
---
+++ src/test/java/Homework17.java
@@ -0,0 +1,36 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Homework17 extends BaseTest {
+    @Test
+    public void addSongToPlaylist() throws InterruptedException{
+       // String ExpectedString = "Added 1 song into \"Test Pro Playlist.\"";
+        String ExpectedString = "Added 1 song into \"Test Pro Playlist.\"";
+        naviagionURL();
+        provideEmail("felicia.clay@testpro.io");
+        providePassword("ACw0FWOe");
+        clickSubmitBtn();
+        Thread.sleep(2000);
+        searchSong("HoliznaCCO");
+        Thread.sleep(2000);
+        clickViewALLBtn();
+        Thread.sleep(2000);
+        selectFirstSong();
+        Thread.sleep(2000);
+        clickAddToBtn();
+        Thread.sleep(2000);
+       // createPlaylist("17SongList");
+       // clickSaveBtn();
+        choosePlaylist();
+        Thread.sleep(5000);
+        Assert.assertEquals(getAddToPlaylistSuccessMsg(),ExpectedString);
+
+
+
+    }
+}
