dbf8ca08d3b9fef53b4fddee9839f7d407482dab
fdclay <felicia.clay@testpro.io>
2025-09-25 22:33:18 -0400

    Added steps to add to playlist

--- src/test/java/Homework17.java
+++ src/test/java/Homework17.java
@@ -1,10 +1,10 @@
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.time.Duration;
+//import java.time.Duration;
 
 public class Homework17 extends BaseTest {
     @Test
