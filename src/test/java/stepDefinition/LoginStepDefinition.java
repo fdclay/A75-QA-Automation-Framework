9e9a7bce138cfb70e67b56aa65e7d2bdd90deb26
fdclay <felicia.clay@testpro.io>
2025-10-27 23:55:41 -0400

    running Cucumber

---
+++ src/test/java/stepDefinition/LoginStepDefinition.java
@@ -0,0 +1,83 @@
+package stepDefinition;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.bs.A;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import pom.HomePage;
+import pom.LoginPage;
+
+import java.time.Duration;
+
+public class LoginStepDefinition {
+
+    WebDriver driver;
+    WebDriverWait wait;
+
+
+    @Given("I open browser")
+    //@Before
+    public void openBrowser(){
+        WebDriverManager.chromiumdriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disabled-notification");
+        options.addArguments("--remote-allow-origines=*");
+        ChromeDriver driver = new ChromeDriver(options);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    @And("I open Login page")
+    public void iOpenLoginPage() {
+        driver.get("https://qa.koel.app");
+    }
+
+    @When("I enter email {string}")
+    public void iEnterEmail(String email) {
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.provideEmail(email);
+
+       /*wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[type]='email']"))).clear();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[type]='email']"))).sendKeys(email);*/
+    }
+
+    @And("I enter password {string}")
+    public void ienterpassword(String password) {
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.providePassword(password);
+    }
+
+    @And("I submit")
+    public void iSubmit() {
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.clickSubmit();
+        //wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[type]='submit']"))).click();
+    }
+
+    @Then("I am logged in")
+    public void iAmLoggedIn() {
+        HomePage homePage = new HomePage(driver);
+        homePage.getUserAvatar();
+        Assert.assertTrue(homePage.getUserAvatar().isDisplayed());
+
+
+        //Assert.assertTrue(wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("img.avatar"))).isDisplayed());
+    }
+
+    @After
+    public void closeBrowser(){
+        driver.quit();
+    }
+}
---
+++ src/test/resources/features/Login.feature
@@ -0,0 +1,26 @@
+Feature: Login Feature
+  #this is for comments
+
+  #Scenario:
+    Given I open browser
+    And I open Login page
+    When I enter email "felicia.clay@testpro.io"
+    And I enter password "ACw0FWOe"
+    And I submit
+    Then I am logged in
+
+  Scenario Outline:
+    Given I open browser
+    And I open Login page
+    When I enter email "<Email>"
+    And I enter password "<Password>"
+    And I submit
+    Then I am logged in
+    Examples:
+      | Email                   | Password  |
+      | felicia.clay@testpro.io | ACw0FWOe  |
+      | demo@testpro.io         | ACw0FWOe  |
+      | felicia.clay@testpro.io | demotest  |
+      | felicia.clay@testpro.io |           |
+      |                         | ACw0FWOe  |
+
